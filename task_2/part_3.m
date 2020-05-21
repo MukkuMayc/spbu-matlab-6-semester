edf6f6f16ba1b8954001483bf51e84d10b691901
MukkuMayc <st063775@student.spbu.ru>
2020-05-21 09:08:37 +0300

    Reorganize plots in part 4 as subplots

--- task_2/part_3.m
+++ task_2/part_3.m
@@ -13,29 +13,33 @@ tspan = [0 1];
 
 x_0 = 1;
 
-% backward_euler_steps(f, tspan, x_0);
+backward_euler_steps(f, tspan, x_0);
 forward_euler_steps(f, tspan, x_0);
 
 function backward_euler_steps(f, tspan, x_0)
     steps = 2.^linspace(1, 10, 10) .* 10;
     
+    subplot(2, 1, 1);
     hold on
     for i = 1:10
         [t, x] = backward_euler(f, tspan, x_0, steps(i));
         plot(t, x, 'DisplayName', "step=" + num2str(steps(i)));
     end
     legend
+    title("backward euler");
     hold off
 end
 
 function forward_euler_steps(f, tspan, x_0)
     steps = linspace(5001, 5010, 10);
     
+    subplot(2, 1, 2);
     hold on
     for i = 1:10
         [t, x] = forward_euler(f, tspan, x_0, steps(i));
         plot(t, x, 'DisplayName', "step=" + num2str(steps(i)));
     end
     legend
+    title("forward euler");
     hold off
 end
